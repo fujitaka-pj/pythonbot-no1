a10265c01c8ed5cf9bdf543871cb41f44cb4d1a4
piruty <tafuku.5119@gmail.com>
2014-12-01 20:14:28 +0900

    オウム返し機能を追加

--- init.py
+++ init.py
@@ -2,19 +2,23 @@
 import sys
 import random
 
-def reply():
+def reply(user_input=""):
   selection = int(random.random() * 3)
   if selection == 0:
     print("How are you??")
   elif selection == 1:
     print("I am fine.")
   else:
-    print(selection)
+    if user_input != "":
+      print(user_input)
+    else:
+      print(selection)
 
-for value in range(3):
-  user_input = sys.stdin.readline()
-  print(user_input)
-  reply()
-  if user_input == 'exit\n':
-    print("Bye")
-    break
+if __name__ == "__main__"
+  for value in range(3):
+    user_input = sys.stdin.readline()
+    print(user_input)
+    reply(user_input)
+    if user_input == 'exit\n':
+      print("Bye")
+      break
